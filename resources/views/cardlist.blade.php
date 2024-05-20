bc36e8a4c1a3850efa9bbe4da71993aa37f28bfd
Francesco-Rianna <maradonino98@hotmail.it>
2024-05-20 18:29:12 +0200

    feat:inserted button

--- resources/views/cardlist.blade.php
+++ resources/views/cardlist.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.app')
 @section('content')
-        <div class="container d-flex flex-wrap py-2 gap-2" >
+    <div class="container">
+        <button class="ms-btn-bg-main p-1">Current series</button>
+        <div class=" d-flex flex-wrap py-2 gap-2" >
             @foreach ($comics as $comic)
                 
-           
+            
             <div class="ms-card ">
                 <img src="{{$comic['thumb']}}" alt="comic-img">
                 <h5> {{$comic['title']}} </h5>
@@ -13,4 +15,6 @@
             @endforeach
             
         </div>
+    </div>
+
 @endsection
