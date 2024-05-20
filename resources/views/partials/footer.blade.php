f0e92b0226703dc9e26ea9e8e0e74addd0a3cd53
Francesco-Rianna <maradonino98@hotmail.it>
2024-05-20 19:42:33 +0200

    feat: added footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,72 @@
+<div class="bg-foot">
+    <div class="container">
+        <div class="d-flex gap-4 py-5 ">
+            <div>
+                <h5 class="tit-foot">DC COMICS</h5>
+                <nav>
+                    <ul class= 'list-unstyled'>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                     </ul>
+                </nav>
+                <h5 class="tit-foot">SHOP</h5>
+            </div>
+
+            <div>
+                <h5 class="tit-foot">DC</h5>
+                <nav>
+                    <ul class= 'list-unstyled'>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                     </ul>
+                </nav>
+            </div>
+    
+           <div>
+             <h5 class="tit-foot">SITES</h5>
+             <nav>
+                    <ul class= 'list-unstyled'>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                     </ul>
+                </nav>
+
+            </div>
+        </div>
+    </div>
+  </div>
+  <div class="bg-foot-down">
+    <div class="container">
+        <div class="d-flex justify-content-between py-4 align-items-center ">
+            <button class="bg-transparent p-2">
+                SIGN-UP-NOW!
+            </button>
+            <div class="d-flex align-items-center gap-2">
+                <span class="">Follow Us</span>
+                <div class="socials">
+                    <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="logo">
+                    <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="logo">
+                    <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="logo">
+                    <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="logo">
+                    <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="logo">
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
