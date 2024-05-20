59f1018742d05059ad879a1ee8beec5c2032476d
Francesco-Rianna <maradonino98@hotmail.it>
2024-05-20 16:41:46 +0200

    feat: added header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+
+<header>
+    <div class="ms-container d-flex align-items-center justify-content-between p-3">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="logo">
+        <nav>
+            <ul class="d-flex gap-2 list-unstyled">
+                <li>Characters</li>
+                <li class="ms-hover">Comics</li>
+                <li>Movies</li>
+                <li>Tv</li>
+                 <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>Fans</li>
+                <li>News</li>
+                <li class="prova">Shop</li>
+            </ul>
+        </nav>
+    </div>
+    
+</header>
